effe121eb8b2d34b062d0eb15fe2773b73d3a700
traveltime07-hash <traveltime07@gmail.com>
2025-09-02 22:20:56 +0200

    Create Objects.tsx

---
+++ src/pages/Objects.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+export function Objects() {
+  return (
+    <div className="card">
+      <h1>Obiekty i pokoje</h1>
+      <p>Lista obiektów, data premium, przycisk „Przejdź na premium”.</p>
+    </div>
+  );
+}
