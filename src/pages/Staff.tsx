451b20d16cf65b719432ebe15ab4f84971a10a6d
traveltime07-hash <traveltime07@gmail.com>
2025-09-02 22:22:24 +0200

    Create Staff.tsx

---
+++ src/pages/Staff.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+export function Staff() {
+  return (
+    <div className="card">
+      <h1>Pracownicy i role</h1>
+      <p>Dodawanie pracowników, role, uprawnienia per obiekt/pokój.</p>
+    </div>
+  );
+}
