1202774f3b6a30be343cd0818066d3b6af587f9f
traveltime07-hash <traveltime07@gmail.com>
2025-09-02 22:20:37 +0200

    Create Home.tsx

---
+++ src/pages/Home.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+export function Home() {
+  return (
+    <div className="card">
+      <h1>Dashboard</h1>
+      <p>Skrót: zadania, zajętość, powiadomienia.</p>
+    </div>
+  );
+}
